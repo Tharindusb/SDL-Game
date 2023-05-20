0b37450f121777f3c35e29f6bc99e701c3ac38c2
Tharindu Buwaneka <buwanekatharindu@gmail.com>
2023-05-21 00:16:53 +0530

    created feature/user-input-handling

--- first_Game_project/src/mainWindow.cpp
+++ first_Game_project/src/mainWindow.cpp
@@ -34,7 +34,10 @@ int main(int argc, char* args[])
 			{
 				while (SDL_PollEvent(&e))
 				{
-					if (e.type == SDL_QUIT) quit = true;
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
 				}
 			}
 		}
