bdd6af1e585c0cd2dd4e8bb32def21e3fca8f417
Tharindu Buwaneka <buwanekatharindu@gmail.com>
2023-05-20 23:17:38 +0530

    initial commit

---
+++ first_Game_project/src/mainWindow.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <SDL.h>
+
+bool init();
+bool loadMedia();
+void close();
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+int main(int argc, char* args[])
+{
+	if (!init()) 
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		if (!loadMedia()) 
+		{
+			printf("Failed to load Media\n");
+		}
+		else
+		{
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+			SDL_Event e;
+			bool quit = false;
+			while (quit == false)
+			{
+				while (SDL_PollEvent(&e))
+				{
+					if (e.type == SDL_QUIT) quit = true;
+				}
+			}
+		}
+	}
+	close();
+	return 0;
+}
+
+bool init() 
+{
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL) {
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia() 
+{
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("src/img/hello_world.bmp");
+	
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "img/hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close() 
+{
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
